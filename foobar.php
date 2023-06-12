240bcbb8c7acc087b1e68d3a2db4ffccdb637f71
bubig <bubiguillaume@gmail.com>
2023-06-12 10:23:50 +1000

    add start and end variable / Format code

--- foobar.php
+++ foobar.php
@@ -1,20 +1,22 @@
 <?php
+$start = 1;
+$end = 100;
 $modFirst = 3;
 $modSecond = 5;
-for ($i = 1; $i <=100; $i++ ){
-    if($i % $modSecond == 0  && $i % $modFirst == 0){
+for ($i = $start; $i <= $end; $i++) {
+    if ($i % $modSecond == 0 && $i % $modFirst == 0) {
         // divided by 5 and 3 (no remainder)
         echo "foobar";
-    }elseif( $i % $modSecond == 0 ){
+    } elseif ($i % $modSecond == 0) {
         // divided by 5
         echo "bar";
-    }else if ($i % $modFirst == 0 ){
+    } elseif ($i % $modFirst == 0) {
         // divided by 3
         echo "foo";
-    }else{
+    } else {
         echo $i;
     }
-    if($i != 100) {
+    if ($i != 100) {
         echo ",";
     }
 }
