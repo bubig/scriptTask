63ee37d69ad44d2764f07c91f8eb210fa4b88ade
bubig <bubiguillaume@gmail.com>
2023-06-12 12:11:50 +1000

    creating SQL sample for later purpose

---
+++ dump.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS CATALYST_TEST;
+ USE CATALYST_TEST;
+CREATE TABLE IF NOT EXISTS users (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    surname varchar(50) NOT NULL,
+    email varchar(255) NOT NULL,
+    UNIQUE KEY email_key (email)
+    )
